b7e40364f9411aa0b872ffa8c09d48b9e99a6bf5
nimaalizade <alzde82@gmail.com>
2026-06-27 00:12:12 +0330

    created Main.java class as user interface with

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import Service.*;
+import Model.*;
+
+import java.sql.SQLException;
+
 public class Main {
-    static void main() {
-        System.out.println("tempaa");
-    }
+   public static void main(String [] args) throws SQLException {
+       CategortService categortService=new CategortService();
+       Category category=new Category(2,"test");
+       int result;
+       result=categortService.add(category);
+       if(result == 0)
+           System.out.println("thisCategoryIdIsExists");
+       if(result==-1)
+           System.out.println("CategorryNotAdded");
+       if(result==1)
+           System.out.println("CategoryAdded");
+   }
 }
